4a1a687673a871d0e8def5be1b17be4cf970ae39
wwb <22379099@qq.com>
2024-04-28 10:23:44 +0800

    1

--- src/WWB.Common/FriendlyException.cs
+++ src/WWB.Common/FriendlyException.cs
@@ -7,30 +7,63 @@ namespace WWB.Common
     /// </summary>
     public class FriendlyException : Exception
     {
+        /// <summary>
+        /// 错误编码
+        /// </summary>
         public int ErrorCode { get; set; }
 
+        /// <summary>
+        /// Ctor
+        /// </summary>
         public FriendlyException() : base()
         {
         }
 
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="message">错误描述</param>
         public FriendlyException(string message) : base(message)
         {
         }
 
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="message">错误描述</param>
+        /// <param name="innerException">内部异常</param>
         public FriendlyException(string message, Exception innerException) : base(message, innerException)
         {
         }
 
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="errorCode">错误编码</param>
+        /// <param name="message">错误描述</param>
         public FriendlyException(int errorCode, string message) : base(message)
         {
             ErrorCode = errorCode;
         }
 
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="errorCode">错误编码</param>
+        /// <param name="message">错误描述</param>
+        /// <param name="innerException">内部异常</param>
         public FriendlyException(int errorCode, string message, Exception innerException) : base(message, innerException)
         {
             ErrorCode = errorCode;
         }
 
+        /// <summary>
+        /// 格式化异常
+        /// </summary>
+        /// <typeparam name="T">错误枚举</typeparam>
+        /// <param name="errorCode">错误编码</param>
+        /// <param name="formatTexts">格式化文本</param>
+        /// <returns></returns>
         public static FriendlyException Of<T>(T errorCode, params string[] formatTexts) where T : Enum
         {
             var error = errorCode.GetDescription();
--- src/WWB.Common/Websockets/IWebsocketMsgHandler.cs
+++ src/WWB.Common/Websockets/IWebsocketMsgHandler.cs
@@ -1,9 +1,24 @@
 ﻿namespace WWB.Common.Websockets
 {
+    /// <summary>
+    /// Websocket消息处理接口
+    /// </summary>
     public interface IWebsocketMsgHandler
     {
+        /// <summary>
+        /// 接收消息
+        /// </summary>
+        /// <param name="fromClientId">客户端标识</param>
+        /// <param name="msg">消息</param>
+        /// <returns></returns>
         Task ReceiveMsg(string fromClientId, string msg);
 
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <param name="toClientId">客户端标识</param>
+        /// <param name="msg">发送消息</param>
+        /// <returns></returns>
         Task SendMsg(string toClientId, string msg);
     }
 }
